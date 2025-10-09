d485808016b599d615e9f691dbdcc9a5e5c267c0
cholis <anoercholis.asia.ac.id>
2025-10-09 12:38:34 +0700

    Initial commit: Proyek  Django Kelaurahan

---
+++ warga/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Warga, Pengaduan
+
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
