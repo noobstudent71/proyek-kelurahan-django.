04f3c4be715af1414a7f888363b7bdc17b7ac85a
cholis <anoercholis.asia.ac.id>
2025-10-16 12:25:27 +0700

    tugas praktikum 3 framework programing

--- warga/views.py
+++ warga/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Warga, Pengaduan
+from .forms import WargaForm, PengaduanForm
 
 class WargaListView(ListView):
     model = Warga
@@ -10,3 +12,15 @@ class WargaDetailView(DetailView):
 
 class PengaduanListView(ListView):
     model = Pengaduan
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list') 
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list') 
