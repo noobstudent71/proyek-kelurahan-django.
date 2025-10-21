910ebf4f7092d8c83205ddd787352018b8e6d415
cholis <anoercholis.asia.ac.id>
2025-10-21 13:56:26 +0700

    tugas pertemuan ke 4

--- warga/urls.py
+++ warga/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView  
+from .views import (
+    WargaListView, WargaDetailView, WargaCreateView, WargaUpdateView, WargaDeleteView,
+    PengaduanListView, PengaduanCreateView, PengaduanUpdateView, PengaduanDeleteView
+)
 
-
-urlpatterns =[
-
+urlpatterns = [
+    # --- URL Warga (Ini sudah benar) ---
     path('', WargaListView.as_view(), name='warga-list'),
     path('tambah/', WargaCreateView.as_view(), name='warga-tambah'),
-    path('<int:pk>/',WargaDetailView.as_view(),name='warga-detail'),
-    path('pengaduan/',PengaduanListView.as_view(), name='pengaduan-list'),
-    path('pengaduan/tambah',PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'),
+    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'),
+    
+    # --- URL Pengaduan ---
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
+    path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan-tambah'), 
+    
+    # 
+    # INI PERBAIKANNYA: Tambahkan 'pengaduan/' di depan path
+    # 
+    path('pengaduan/<int:pk>/edit/', PengaduanUpdateView.as_view(), name='pengaduan-edit'),
+    path('pengaduan/<int:pk>/hapus/', PengaduanDeleteView.as_view(), name='pengaduan-hapus'),
 ]
--- warga/views.py
+++ warga/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
 
@@ -24,3 +24,26 @@ class PengaduanCreateView(CreateView):
     form_class = PengaduanForm
     template_name = 'warga/pengaduan_form.html'
     success_url = reverse_lazy('pengaduan-list') 
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html' # Kita pakai template yang sama
+    success_url = reverse_lazy('warga-list')
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html' # Kita pakai template yang sama
+    success_url = reverse_lazy('pengaduan-list')
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
+    success_url = reverse_lazy('pengaduan-list')
+
