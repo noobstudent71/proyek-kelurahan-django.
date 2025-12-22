cc05bf6553051a20bd543c7f7c08f5d78ccf2ae8
cholis <anoercholis.asia.ac.id>
2025-12-22 14:40:46 +0700

    comit pertermuan ke 9

--- keluarahan/settings.py
+++ keluarahan/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'warga',
     'rest_framework',
+    'rest_framework.authtoken',
+     'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, # Jumlah item per halaman
+
+}         
--- warga/views.py
+++ warga/views.py
@@ -1,26 +1,51 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
+
+# --- IMPOR DRF ---
 from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .serializers import WargaSerializer,PengaduanSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .serializers import WargaSerializer, PengaduanSerializer
 
-#class WargaListAPIView(ListAPIView):
-    #queryset = Warga.objects.all()
-    #serializer_class = WargaSerializer
+# ===========================
+#        API VIEWS
+# ===========================
 
-#class WargaDetailAPIView(RetrieveAPIView):
-    #queryset = Warga.objects.all()
-    #serializer_class = WargaSerializer
-                         
 class WargaViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Warga.objects.all().order_by('-tanggal_registrasi')
     serializer_class = WargaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    # Fitur Lanjutan
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['nama_lengkap', 'nik', 'alamat']
+    ordering_fields = ['nama_lengkap', 'tanggal_registrasi']
+
+
+class PengaduanViewSet(viewsets.ModelViewSet):
+    queryset = Pengaduan.objects.all()
+    serializer_class = PengaduanSerializer
+
+    permission_classes = [IsAdminUser]
+    
+    # --- JAWABAN TANTANGAN ---
+    # Menambahkan fitur pencarian dan pengurutan pada Pengaduan
+    filter_backends = [SearchFilter, OrderingFilter]
+    
+    # Bisa mencari berdasarkan Judul atau Isi Deskripsi
+    search_fields = ['judul', 'deskripsi']
+    
+    # Bisa mengurutkan berdasarkan Status atau Judul
+    ordering_fields = ['status', 'judul']
+
+
+# ===========================
+#    HTML VIEWS (DJANGO)
+# ===========================
 
 class WargaListView(ListView):
     model = Warga
@@ -46,7 +71,7 @@ class PengaduanCreateView(CreateView):
 class WargaUpdateView(UpdateView):
     model = Warga
     form_class = WargaForm
-    template_name = 'warga/warga_form.html' # Kita pakai template yang sama
+    template_name = 'warga/warga_form.html'
     success_url = reverse_lazy('warga-list')
 
 class WargaDeleteView(DeleteView):
@@ -57,15 +82,10 @@ class WargaDeleteView(DeleteView):
 class PengaduanUpdateView(UpdateView):
     model = Pengaduan
     form_class = PengaduanForm
-    template_name = 'warga/pengaduan_form.html' # Kita pakai template yang sama
+    template_name = 'warga/pengaduan_form.html'
     success_url = reverse_lazy('pengaduan-list')
 
 class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')
-
-class PengaduanViewSet(viewsets.ModelViewSet):
-    queryset = Pengaduan.objects.all()
-    serializer_class = PengaduanSerializer
-
